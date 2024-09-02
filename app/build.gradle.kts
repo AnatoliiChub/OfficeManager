c1ea58d0291d18a0d495308afce92d90d4991e79
anatoliy <anatoliy.chub.dev@gmail.com>
2024-09-02 11:26:31 +0200

    List Item Placement animation implemented.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,11 +74,13 @@ dependencies {
     implementation(libs.room)
     ksp(libs.room.compiler)
     implementation(libs.androidx.room.ktx)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
+    
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 }
--- app/src/main/java/com/chub/officemanager/ui/view/OfficeItemsList.kt
+++ app/src/main/java/com/chub/officemanager/ui/view/OfficeItemsList.kt
@@ -1,13 +1,17 @@
 package com.chub.officemanager.ui.view
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.chub.officemanager.ui.ItemOperation
 import com.chub.officemanager.util.OfficeItem
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun OfficeItemsList(
     list: List<OfficeItem>,
@@ -21,11 +25,13 @@ fun OfficeItemsList(
         contentPadding = PaddingValues(horizontal = horizontalPadding, vertical = verticalPadding)
     ) {
         items(list.size, key = { list[it].id }) { index ->
-            OfficeItemLayout(
-                list[index],
-                operations,
-                onClick = { onOperation(it) }
-            )
+            Box(modifier = Modifier.animateItemPlacement()) {
+                OfficeItemLayout(
+                    list[index],
+                    operations,
+                    onClick = { onOperation(it) }
+                )
+            }
         }
     }
 }
